91d87c9f27df8aa62520950b347389decd752651
Arman11980 <165942302+Arman11980@users.noreply.github.com>
2024-07-08 21:28:20 +0300

    Update legacy_class.py

--- legacy_class.py
+++ legacy_class.py
@@ -1,16 +1,46 @@
 class Animal:
-    def __init__(self):
-        self.name = name
-        self.alive = True
-        self.fed = False
+    alive = True
+    fed = False
+    name = None
+
+    def eat(self, food):
+        if food.edible:
+            self.fed = True
+            print(f'{self.name} съел {food.name}')
+        else:
+            self.alive = False
+            print(f'{self.name} не стал есть {food.name}')
+
 class Mammal(Animal):
-    def eat(self, food):
-        self.food = food
+    def __init__(self,name):
+        self.name = name
+
 class Predator(Animal):
-    def eat(self, food):
-        self.food = food
+    def __init__(self,name):
+        self.name = name
 
 class Plant:
-    def __init__(self):
+    edible = False
+    def __init__(self, name):
         self.name = name
-        self.edible = False
+
+class Flower(Plant):
+    edible = False
+
+class Fruit(Plant):
+    edible = True
+
+a1 = Predator('Волк с Уолт-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
