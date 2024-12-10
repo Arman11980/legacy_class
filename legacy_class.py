7d8e8b1226fdbc01863545c5bd64c441b843430f
Arman <165942302+Arman11980@users.noreply.github.com>
2024-12-10 20:52:10 +0300

    Update legacy_class.py

--- legacy_class.py
+++ legacy_class.py
@@ -1,43 +1,46 @@
 class Animal:
-    alive = True
-    fed = False
-    name = None
-
-    def eat(self, food):
-        if food.edible:
-            self.fed = True
-            print(f'{self.name} съел {food.name}')
-        else:
-            self.alive = False
-            print(f'{self.name} не стал есть {food.name}')
-
-class Mammal(Animal):
-    def __init__(self,name):
-        self.name = name
-
-class Predator(Animal):
-    def __init__(self,name):
+    def __init__(self, name):
+        self.alive = True
+        self.fed = False
         self.name = name
 
 class Plant:
-    edible = False
     def __init__(self, name):
+        self.edible = False
         self.name = name
 
+class Mammal(Animal):
+    def eat(self, food):
+        if food.edible:
+            print(f'{self.name} съел {food.name}')
+            self.fed = True
+        else:
+            print(f'{self.name} не стал есть {food.name}')
+            self.alive = False
+
+class Predator(Animal):
+    def eat(self, food):
+        if food.edible:
+            print(f'{self.name} съел {food.name}')
+            self.fed = True
+        else:
+            print(f'{self.name} не стал есть {food.name}')
+            self.alive = False
+
 class Flower(Plant):
-    edible = False
+    pass
 
 class Fruit(Plant):
-    edible = True
+    def __init__(self, name):
+        super().__init__(name)
+        self.edible = True
 
-a1 = Predator('Волк с Уолт-Стрит')
+a1 = Predator('Волк с Уолл-Стрит')
 a2 = Mammal('Хатико')
 p1 = Flower('Цветик семицветик')
 p2 = Fruit('Заводной апельсин')
-
 print(a1.name)
 print(p1.name)
-
 print(a1.alive)
 print(a2.fed)
 a1.eat(p1)
